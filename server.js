62829e2a9b1d87e2c47eef3360b3dbca046652db
Twarimitswe-Aaron <aarontwarimitswe@gmail.com>
2025-04-02 18:32:22 +0200

    [New knowledge]: updating data

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app=express();
 const courses=[
     {id:1,name:"Eng"},
 ]
+
+//Getting data in general and by specific id
 app.use(express.json());
 app.get('/', (req,res)=>{
     res.send(courses);
@@ -16,4 +18,42 @@ app.get('/:id', (req,res)=>{
     res.send(course);
 })
 
-app.listen(3000);
+app.post('/newCourse', (req,res)=>{
+    const {error}=ValidateCourse(req.body)
+    if(error){
+        res.status(404).send(error.details[0].message);
+        return
+    }
+    const course={
+        id:courses.length+1,
+        name:req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+
+})
+
+app.put('/updateCourse/:id', (req,res)=>{
+    const course=courses.find(c=>c.id===parseInt(req.params.id));
+    if(!course){res.status(404).send("course is not available");return};
+
+    const {error}=ValidateCourse(req.body);
+    if(error){
+        res.status(404).send(error)
+        return;
+    }
+    course.name=req.body.name;
+    res.status(200).send(course);
+})
+
+function ValidateCourse(data){
+    const result=Joi.object({
+        name:Joi.string().required().min(3)
+    })
+
+    return result.validate(data);
+}
+
+app.listen(3000,()=>{
+    console.log("Server is runningg.....at http://localhost:3000");
+});
